ffcbd0f11c6d26bb228e5df4aca6af751bf537bd
~egoist <sahoosibaprasad2221@gmail.com>
2024-03-12 16:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function makeid(l) {
 	const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	let result = ' ';
     const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
+    for ( let i = 0; i < l; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
 	return result;
